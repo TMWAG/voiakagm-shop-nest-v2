bb34cbc1a621ff3be134e5292f6eb9312c5be6d0
TMWAG <semyon.valyaev@gmail.com>
2023-03-02 01:33:58 +0300

    изменено название env файла

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.local.env`,
+      envFilePath: `.env`,
     }),
   ],
   controllers: [],
