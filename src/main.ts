041f3a3d6ff480a4864feff68eae7ec2b9b99340
TMWAG <semyon.valyaev@gmail.com>
2023-03-01 23:54:05 +0300

    сконфигурирован Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const port = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('VoiakaGM-shop-V2')
+    .setDescription('Документация API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+  await app.listen(port, () => console.log(`server started at ${port} port`));
 }
 bootstrap();
