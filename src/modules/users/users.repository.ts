b727eb38d08bceb2a73cf4907bd86c854ca96c8f
TMWAG <semyon.valyaev@gmail.com>
2023-03-20 20:54:18 +0300

    CRUD функционал в репозитории user

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
@@ -7,14 +7,36 @@ export class UsersRepository {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({ data });
+    return await this.prisma.user.create({ data });
   }
 
   async getUserById(id: number): Promise<User | never> {
-    return this.prisma.user.findFirstOrThrow({ where: { id } });
+    return await this.prisma.user.findFirstOrThrow({ where: { id } });
   }
 
-  // async getUserByPhone(
-  //   where: Prisma.UserWhereUniqueInput,
-  // ): Promise<>
+  async getUserByPhone(phone: string): Promise<User | never> {
+    return await this.prisma.user.findFirstOrThrow({ where: { phone } });
+  }
+
+  async getUserByEmail(email: string): Promise<User | never> {
+    return await this.prisma.user.findFirstOrThrow({ where: { email } });
+  }
+
+  async updateUserTgLinkById(
+    tgLink: string,
+    id: number,
+  ): Promise<User | never> {
+    return await this.prisma.user.update({ where: { id }, data: { tgLink } });
+  }
+
+  async updateUserVkLinkById(
+    vkLink: string,
+    id: number,
+  ): Promise<User | never> {
+    return await this.prisma.user.update({ where: { id }, data: { vkLink } });
+  }
+
+  async deleteUserById(id: number): Promise<User | never> {
+    return await this.prisma.user.delete({ where: { id } });
+  }
 }
