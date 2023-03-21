2948b8f9b24c9cba7bf64ccb79172e5d1d78eb53
TMWAG <semyon.valyaev@gmail.com>
2023-03-21 23:00:31 +0300

    добавлены async/await, где были нужны, новые методы

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,30 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
   constructor(private repository: UsersRepository) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.repository.createUser(createUserDto);
+  async create(createUserDto: CreateUserDto) {
+    return await this.repository.createUser(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async getAll() {
+    return await this.repository.getAllUsers();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async getOneUserById(id: number) {
+    return await this.repository.getUserById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async getUserByEmail(email: string) {
+    return await this.repository.getUserByEmail(email);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async getOneUserByIdOrThrowError(id: number) {
+    const user = await this.repository.getUserById(id);
+    if (!user) throw new NotFoundException();
+    return user;
+  }
+
+  async getUserByEmailOrThrowError(email: string) {
+    const user = await this.repository.getUserByEmail(email);
+    if (!user) throw new NotFoundException();
+    return user;
   }
 
   //utils
