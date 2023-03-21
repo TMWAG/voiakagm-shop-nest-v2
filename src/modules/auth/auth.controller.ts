4382629dce5a1c5fb3f9dc42a473ce647dc483e7
TMWAG <semyon.valyaev@gmail.com>
2023-03-21 23:11:26 +0300

    сервис: методы валидации, логина, регистравции и проверки уникальности телефона и почты; модуль: импортированы модули паспорта, пользователей и jwt, в провайдеры добавлена jwtStategy; контроллер: методы для регистрации и авторизации

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserByEmailDto } from './dto/login-user-by-email.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() dto: RegisterUserDto) {
+    return this.authService.registerUser(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginUserByEmailDto) {
+    return this.authService.login(dto);
+  }
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserError } from 'src/errors/user.errors';
+import { UsersService } from '../users/users.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { LoginUserByEmailDto } from './dto/login-user-by-email.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async registerUser(dto: RegisterUserDto): Promise<true | never> {
+    await this.checkUserCredentialsUniquenessOrThrowError(dto.email, dto.phone);
+    const hashedPassword = await bcrypt.hash(dto.password, 5);
+    await this.usersService.create({
+      ...dto,
+      password: hashedPassword,
+    });
+    return true;
+  }
+
+  async login(dto: LoginUserByEmailDto) {
+    const user = await this.usersService.getUserByEmailOrThrowError(dto.email);
+    const payload = { id: user.id, email: user.email, role: user.role };
+    return {
+      access_token: this.jwtService.sign(payload, {
+        secret: process.env.SECRET_KEY,
+      }),
+    };
+  }
+
+  //utils
+  async validateUser(
+    email: string,
+    pass: string,
+  ): Promise<Partial<User> | null> {
+    const user = await this.usersService.getUserByEmail(email);
+    if (await bcrypt.compare(pass, user.password)) {
+      const { password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+
+  private async checkUserCredentialsUniquenessOrThrowError(
+    email: string,
+    phone: string,
+  ): Promise<void | never> {
+    if (!(await this.usersService.isUserEmailNew(email))) {
+      throw new HttpException(
+        UserError.notUnique.email,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    if (!(await this.usersService.isUserPhoneNew(phone))) {
+      throw new HttpException(
+        UserError.notUnique.phone,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
