839db335be2b22f605f94072fd2e1f52fe1ed682
TMWAG <semyon.valyaev@gmail.com>
2023-03-23 23:43:15 +0300

    базовые CRUD операции

---
+++ src/modules/users-addresses/users-addresses.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, UserAddress } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UserAddressRepository {
+  constructor(private prisma: PrismaService) {}
+
+  //create
+  async createUserAddress(
+    data: Prisma.UserAddressCreateInput,
+  ): Promise<UserAddress> {
+    return await this.prisma.userAddress.create({ data });
+  }
+
+  //get
+  async getAllAddressesByUserId(userId: number): Promise<UserAddress[]> {
+    return await this.prisma.userAddress.findMany({ where: { userId } });
+  }
+
+  async getOneById(id: number): Promise<UserAddress> {
+    return await this.prisma.userAddress.findFirst({ where: { id } });
+  }
+
+  //update
+  async updateAddressById(id: number, address: string): Promise<UserAddress> {
+    return await this.prisma.userAddress.update({
+      where: { id },
+      data: { address },
+    });
+  }
+
+  //delete
+  async deleteAddressById(id: number): Promise<UserAddress> {
+    return await this.prisma.userAddress.delete({ where: { id } });
+  }
+}
