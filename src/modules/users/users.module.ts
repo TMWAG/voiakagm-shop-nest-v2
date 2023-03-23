3d0fb3e2fb0122e14c34cb69045efdf1bbcccd96
TMWAG <semyon.valyaev@gmail.com>
2023-03-23 18:43:33 +0300

    импорт JwtService

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,11 +3,18 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/database/prisma.module';
 import { UsersRepository } from './users.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersRepository, UsersService],
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
