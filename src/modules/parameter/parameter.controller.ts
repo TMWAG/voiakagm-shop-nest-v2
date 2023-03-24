4eed852d280f56fd41c36aa5bebc7d0b9088036d
TMWAG <semyon.valyaev@gmail.com>
2023-03-24 22:05:38 +0300

    новые энпоинты в контроллере и импорт JwtModule в модуле

--- src/modules/parameter/parameter.controller.ts
+++ src/modules/parameter/parameter.controller.ts
@@ -1,7 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/decorators/roles-auth.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { CreateParameterDto } from './dto/create-parameter.dto';
+import { DeleteParameterDto } from './dto/delete-parameter.dto';
+import { GetAllParametersByCategoryId } from './dto/get-all-parameters-by-category-id.dto';
+import { UpdateParameterCategoryByIdDto } from './dto/update-parameter-category.dto';
+import { UpdateParameterNameByIdDto } from './dto/update-parameter-name.dto';
 import { ParameterService } from './parameter.service';
 
 @Controller('parameter')
 export class ParameterController {
   constructor(private readonly parameterService: ParameterService) {}
+
+  @Post('add')
+  @Roles(Role.ADMIN, Role.SUPERVISOR)
+  @UseGuards(RolesGuard)
+  createParameter(@Body() dto: CreateParameterDto) {
+    return this.parameterService.createParameter(dto);
+  }
+
+  @Get('all')
+  getAllParameters() {
+    return this.parameterService.getAllParameters();
+  }
+
+  @Get('all')
+  getAllParametersByCategoryId(@Query() dto: GetAllParametersByCategoryId) {
+    return this.parameterService.getAllParametersByCategoryId(dto);
+  }
+
+  @Patch('name')
+  @Roles(Role.ADMIN, Role.SUPERVISOR)
+  @UseGuards(RolesGuard)
+  updateParameterName(@Body() dto: UpdateParameterNameByIdDto) {
+    return this.parameterService.updateParameterNameById(dto);
+  }
+
+  @Patch('category')
+  @Roles(Role.ADMIN, Role.SUPERVISOR)
+  @UseGuards(RolesGuard)
+  updateParameterCategory(@Body() dto: UpdateParameterCategoryByIdDto) {
+    return this.parameterService.updateParameterCategoryById(dto);
+  }
+
+  @Delete('delete')
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  deleteParameter(@Body() dto: DeleteParameterDto) {
+    return this.parameterService.deleteParameterById(dto);
+  }
 }
--- src/modules/parameter/parameter.module.ts
+++ src/modules/parameter/parameter.module.ts
@@ -3,10 +3,17 @@ import { ParameterService } from './parameter.service';
 import { ParameterController } from './parameter.controller';
 import { ParameterRepository } from './parameter.repository';
 import { PrismaModule } from 'src/database/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [ParameterService, ParameterRepository],
   controllers: [ParameterController],
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
 })
 export class ParameterModule {}
