977578d0532f81abb0804b494e723d28cce68c55
TMWAG <semyon.valyaev@gmail.com>
2023-03-24 15:08:23 +0300

    базовые CRUD операции

--- src/modules/category/category.repository.ts
+++ src/modules/category/category.repository.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class CategoryRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createCategory(name: string): Promise<Category> {
+    return await this.prisma.category.create({ data: { name } });
+  }
+
+  async getAllCategories(): Promise<Category[]> {
+    return await this.prisma.category.findMany();
+  }
+
+  async getCategoryById(id: number): Promise<Category | undefined> {
+    return await this.prisma.category.findFirst({ where: { id } });
+  }
+
+  async updateCategoryById(id: number, name: string): Promise<Category> {
+    return this.prisma.category.update({ where: { id }, data: { name } });
+  }
+
+  async deleteCategoryById(id: number): Promise<Category> {
+    return this.prisma.category.delete({ where: { id } });
+  }
 }
