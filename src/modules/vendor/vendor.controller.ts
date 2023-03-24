eb2ade9fa5db4c35f316214cf558385c016d8a3c
TMWAG <semyon.valyaev@gmail.com>
2023-03-24 18:19:33 +0300

    модуль: импортирован JwtModule; контроллер: новые эндпоинты

--- src/modules/vendor/vendor.controller.ts
+++ src/modules/vendor/vendor.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/decorators/roles-auth.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { CreateVendorDto } from './dto/create-vendor.dto';
+import { DeleteVendorDto } from './dto/delete-vendor.dto';
+import { UpdateVendorNameDto } from './dto/update-vendor-name.dto';
 import { VendorService } from './vendor.service';
 
 @Controller('vendor')
 export class VendorController {
   constructor(private readonly vendorService: VendorService) {}
+
+  @Post('add')
+  @Roles(Role.ADMIN, Role.SUPERVISOR)
+  @UseGuards(RolesGuard)
+  crateVendor(@Body() dto: CreateVendorDto) {
+    return this.vendorService.createVendor(dto);
+  }
+
+  @Get('all')
+  getAllVendors() {
+    return this.vendorService.getAllVendors();
+  }
+
+  @Patch('update')
+  @Roles(Role.ADMIN, Role.SUPERVISOR)
+  @UseGuards(RolesGuard)
+  updateVendorName(@Body() dto: UpdateVendorNameDto) {
+    return this.vendorService.updateVendorName(dto);
+  }
+
+  @Delete('delete')
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  deleteVendor(@Body() dto: DeleteVendorDto) {
+    return this.vendorService.deleteVendor(dto);
+  }
 }
--- src/modules/vendor/vendor.module.ts
+++ src/modules/vendor/vendor.module.ts
@@ -3,10 +3,17 @@ import { VendorService } from './vendor.service';
 import { VendorController } from './vendor.controller';
 import { VendorRepository } from './vendor.repository';
 import { PrismaModule } from 'src/database/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [VendorService, VendorRepository],
   controllers: [VendorController],
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
 })
 export class VendorModule {}
