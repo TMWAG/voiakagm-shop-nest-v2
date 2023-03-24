a89ed9a8612e59e0aee26594d662c42bd31cd29f
TMWAG <semyon.valyaev@gmail.com>
2023-03-24 14:13:05 +0300

    новые CRUD операции и утилитарный метод

--- src/modules/users-addresses/users-addresses.service.ts
+++ src/modules/users-addresses/users-addresses.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserAddress } from '@prisma/client';
+import { CreateUserAddressDto } from './dto/create-user-address.dto';
+import { DeleteUserAddressDto } from './dto/delete-user-address.dto';
+import { UpdateUserAddressDto } from './dto/update-user-address.dto';
+import { UserAddressRepository } from './users-addresses.repository';
 
 @Injectable()
-export class UsersAddressesService {}
+export class UsersAddressesService {
+  constructor(private readonly repository: UserAddressRepository) {}
+
+  //create
+  async createUserAddress(userId: number, dto: CreateUserAddressDto) {
+    return await this.repository.createUserAddress(userId, dto.address);
+  }
+
+  //get
+  async getAllUserAddressesByUserId(id: number) {
+    return await this.repository.getAllAddressesByUserId(id);
+  }
+
+  //update
+  async updateUserAddressById(userId: number, dto: UpdateUserAddressDto) {
+    await this.checkUserOwnershipOnAddressOrThrowError(userId, dto.id);
+    return await this.repository.updateAddressById(dto.id, dto.address);
+  }
+
+  //delete
+  async deleteUserAddressById(userId: number, dto: DeleteUserAddressDto) {
+    await this.checkUserOwnershipOnAddressOrThrowError(userId, dto.id);
+    return await this.repository.deleteAddressById(dto.id);
+  }
+
+  //utils
+  async checkUserOwnershipOnAddressOrThrowError(
+    userId: number,
+    addressId: number,
+  ): Promise<UserAddress | never> {
+    const addresses = await this.getAllUserAddressesByUserId(userId);
+    const target = addresses.find((address) => address.id === addressId);
+    if (!target)
+      throw new NotFoundException(
+        `Адрес с Id: ${addressId} не числится за данным пользователем`,
+      );
+    return target;
+  }
+}
