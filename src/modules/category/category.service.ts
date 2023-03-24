2555984a0625358e5de63010d965eb7c02eaf8c5
TMWAG <semyon.valyaev@gmail.com>
2023-03-24 15:51:32 +0300

    новые CRUD методы

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { CategoryRepository } from './category.repository';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { DeleteCategoryDto } from './dto/delete-category.dto';
+import { UpdateCategoryNameDto } from './dto/update-category-name.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly repository: CategoryRepository) {}
+
+  //create
+  async createCategory(dto: CreateCategoryDto) {
+    return await this.repository.createCategory(dto.name);
+  }
+
+  //get
+  async getAllCategories() {
+    return await this.repository.getAllCategories();
+  }
+
+  //update
+  async updateCategoryNameById(dto: UpdateCategoryNameDto) {
+    return await this.repository.updateCategoryById(dto.id, dto.name);
+  }
+
+  //delete
+  async deleteCategoryById(dto: DeleteCategoryDto) {
+    return await this.repository.deleteCategoryById(dto.id);
+  }
+}
