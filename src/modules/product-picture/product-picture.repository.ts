7b0885d5fb5ef1dad43ee0fcc9920eee76107898
TMWAG <semyon.valyaev@gmail.com>
2023-03-28 19:07:05 +0300

    добавление и удаление

--- src/modules/product-picture/product-picture.repository.ts
+++ src/modules/product-picture/product-picture.repository.ts
@@ -1,7 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ProductPicture } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class ProductPictureRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  //create
+  async createProductPicture(
+    productId: number,
+    filename: string,
+  ): Promise<ProductPicture> {
+    return await this.prisma.productPicture.create({
+      data: {
+        filename,
+        product: {
+          connect: {
+            id: Number(productId),
+          },
+        },
+      },
+    });
+  }
+
+  //delete
+  async deleteProductPictureById(id: number): Promise<ProductPicture> {
+    return await this.prisma.productPicture.delete({ where: { id } });
+  }
 }
