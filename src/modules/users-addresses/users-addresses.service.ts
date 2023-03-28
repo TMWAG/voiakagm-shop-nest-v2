ab7b2363742f0633cd1ffa8a0a10bdeac09c1546
TMWAG <semyon.valyaev@gmail.com>
2023-03-28 21:27:18 +0300

    Новый метод getUserAddresByIdOrThrowError

--- src/modules/users-addresses/users-addresses.service.ts
+++ src/modules/users-addresses/users-addresses.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserAddress } from '@prisma/client';
+import { httpExceptionMessages } from 'src/errors/http-exceptions.errors';
 import { CreateUserAddressDto } from './dto/create-user-address.dto';
 import { DeleteUserAddressDto } from './dto/delete-user-address.dto';
 import { UpdateUserAddressDto } from './dto/update-user-address.dto';
@@ -19,14 +20,25 @@ export class UsersAddressesService {
     return await this.repository.getAllAddressesByUserId(id);
   }
 
+  async getUserAddressByIdOrThrowError(id: number) {
+    const userAddress = await this.repository.getOneById(id);
+    if (!userAddress)
+      throw new NotFoundException(
+        httpExceptionMessages.notFound.userAddress(id),
+      );
+    return userAddress;
+  }
+
   //update
   async updateUserAddressById(userId: number, dto: UpdateUserAddressDto) {
+    await this.getUserAddressByIdOrThrowError(dto.id);
     await this.checkUserOwnershipOnAddressOrThrowError(userId, dto.id);
     return await this.repository.updateAddressById(dto.id, dto.address);
   }
 
   //delete
   async deleteUserAddressById(userId: number, dto: DeleteUserAddressDto) {
+    await this.getUserAddressByIdOrThrowError(dto.id);
     await this.checkUserOwnershipOnAddressOrThrowError(userId, dto.id);
     return await this.repository.deleteAddressById(dto.id);
   }
