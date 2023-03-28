c6bacd9f6ed8ea85b4847cab59877b3b6848eb4a
TMWAG <semyon.valyaev@gmail.com>
2023-03-28 20:56:19 +0300

    новый метод getCategoryByIdOrThrowError

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { httpExceptionMessages } from 'src/errors/http-exceptions.errors';
 import { CategoryRepository } from './category.repository';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { DeleteCategoryDto } from './dto/delete-category.dto';
@@ -18,13 +19,22 @@ export class CategoryService {
     return await this.repository.getAllCategories();
   }
 
+  async getCategoryByIdOrThrowError(id: number) {
+    const category = await this.repository.getCategoryById(id);
+    if (!category)
+      throw new NotFoundException(httpExceptionMessages.notFound.category(id));
+    return category;
+  }
+
   //update
   async updateCategoryNameById(dto: UpdateCategoryNameDto) {
+    await this.getCategoryByIdOrThrowError(dto.id);
     return await this.repository.updateCategoryById(dto.id, dto.name);
   }
 
   //delete
   async deleteCategoryById(dto: DeleteCategoryDto) {
+    await this.getCategoryByIdOrThrowError(dto.id);
     return await this.repository.deleteCategoryById(dto.id);
   }
 }
