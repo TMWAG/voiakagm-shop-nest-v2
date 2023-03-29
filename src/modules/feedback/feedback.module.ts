2a8d8028e40bc918a46cd9cf28b502314a4e04b7
TMWAG <semyon.valyaev@gmail.com>
2023-03-29 21:10:41 +0300

    импорт модулей пользователей и товаров

--- src/modules/feedback/feedback.module.ts
+++ src/modules/feedback/feedback.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/database/prisma.module';
+import { ProductModule } from '../product/product.module';
+import { UsersModule } from '../users/users.module';
 import { FeedbackController } from './feedback.controller';
 import { FeedbackRepository } from './feedback.repository';
 import { FeedbackService } from './feedback.service';
@@ -7,6 +9,6 @@ import { FeedbackService } from './feedback.service';
 @Module({
   controllers: [FeedbackController],
   providers: [FeedbackService, FeedbackRepository],
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProductModule, UsersModule],
 })
 export class FeedbackModule {}
