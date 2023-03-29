9fe872037ff7df597962b883333c9d5172f25775
TMWAG <semyon.valyaev@gmail.com>
2023-03-29 17:13:03 +0300

    базовые CRUD операции

--- src/modules/feedback/feedback.repository.ts
+++ src/modules/feedback/feedback.repository.ts
@@ -1,7 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { ProductFeedback } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class FeedbackRepository {
   constructor(private readonly prisma: PrismaService) {}
+  //create
+  async createFeedback(
+    userId: number,
+    productId: number,
+    text: string,
+    rating: number,
+  ): Promise<ProductFeedback> {
+    return await this.prisma.productFeedback.create({
+      data: { userId, productId, text, rating },
+    });
+  }
+
+  //get
+  async getFeedbackById(id: number): Promise<ProductFeedback> {
+    return this.prisma.productFeedback.findFirst({ where: { id } });
+  }
+
+  async getAllFeedbacksByProductId(
+    productId: number,
+  ): Promise<ProductFeedback[]> {
+    return await this.prisma.productFeedback.findMany({ where: { productId } });
+  }
+
+  async getAllFeedbacksByUserId(userId: number): Promise<ProductFeedback[]> {
+    return await this.prisma.productFeedback.findMany({ where: { userId } });
+  }
+
+  //update
+  async updateFeedbackById(
+    id: number,
+    text: string,
+    rating: number,
+  ): Promise<ProductFeedback> {
+    return await this.prisma.productFeedback.update({
+      where: { id },
+      data: { text, rating },
+    });
+  }
+
+  //delete
+  async deleteFeedbackById(id: number): Promise<ProductFeedback> {
+    return await this.prisma.productFeedback.delete({ where: { id } });
+  }
 }
