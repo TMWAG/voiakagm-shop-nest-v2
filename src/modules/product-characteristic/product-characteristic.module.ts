8bfe4905b7e24a81f804ca72151857d076a2fc48
TMWAG <semyon.valyaev@gmail.com>
2023-03-30 01:34:51 +0300

    импорты модулей

--- src/modules/product-characteristic/product-characteristic.module.ts
+++ src/modules/product-characteristic/product-characteristic.module.ts
@@ -3,10 +3,21 @@ import { ProductCharacteristicService } from './product-characteristic.service';
 import { ProductCharacteristicController } from './product-characteristic.controller';
 import { ProductCharacteristicRepository } from './product-characteristic.repository';
 import { PrismaModule } from 'src/database/prisma.module';
+import { ProductModule } from '../product/product.module';
+import { ParameterModule } from '../parameter/parameter.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [ProductCharacteristicService, ProductCharacteristicRepository],
   controllers: [ProductCharacteristicController],
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ProductModule,
+    ParameterModule,
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
 })
 export class ProductCharacteristicModule {}
