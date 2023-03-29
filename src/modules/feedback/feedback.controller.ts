eaa4d5ebf515ff45263de98520f4bd57c803ba1c
TMWAG <semyon.valyaev@gmail.com>
2023-03-29 22:32:08 +0300

    Документация swagger

--- src/modules/feedback/feedback.controller.ts
+++ src/modules/feedback/feedback.controller.ts
@@ -9,6 +9,12 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 import { DeleteFeedbackDto } from './dto/delete-feedback.dto';
@@ -21,28 +27,64 @@ import { FeedbackService } from './feedback.service';
 export class FeedbackController {
   constructor(private readonly feedbackService: FeedbackService) {}
 
+  @ApiOperation({ summary: 'Создание отзыва' })
+  @ApiOkResponse({ description: 'Отзыв успешно добавлен' })
+  @ApiBadRequestResponse({
+    description: 'Не указан id товара и/или текст и/или оценка',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Нет токена авторизации',
+  })
   @Post('create')
   @UseGuards(JwtAuthGuard)
   createFeedback(@Request() { user }, @Body() dto: CreateFeedbackDto) {
     return this.feedbackService.create(user.id, dto);
   }
 
+  @ApiOperation({ summary: 'Получение всех отзывов товара' })
+  @ApiOkResponse({
+    description: 'Успешное получение массива всех отзывов товара',
+  })
+  @ApiBadRequestResponse({
+    description: 'Не указан id товара',
+  })
   @Get('product')
   getAllProductFeedbacks(@Query() dto: GetAllFeedbackByProductIdDto) {
     return this.feedbackService.getAllFeedbacksByProductId(dto);
   }
 
+  @ApiOperation({ summary: 'Получение всех отзывов пользователя' })
+  @ApiOkResponse({
+    description: 'Успешное получение массива всех отзывов пользователя',
+  })
+  @ApiBadRequestResponse({
+    description: 'Не указан id пользователя',
+  })
   @Get('user')
   getAllUserFeedbacks(@Query() dto: GetAllFeedbackByUserIdDto) {
     return this.feedbackService.getAllFeedbacksByUserId(dto);
   }
 
+  @ApiOperation({ summary: 'Изменение текста и/или оценки отзыва' })
+  @ApiOkResponse({ description: 'Отзыв успешно обновлён' })
+  @ApiBadRequestResponse({
+    description: 'Не указан id отзыва и оценка и/или текст',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Нет токена авторизации',
+  })
   @Patch('update')
   @UseGuards(JwtAuthGuard)
   updateFeedback(@Request() { user }, @Body() dto: UpdateFeedbackDto) {
     return this.feedbackService.updateFeedbackById(user.id, dto);
   }
 
+  @ApiOperation({ summary: 'Удаление отзыва' })
+  @ApiOkResponse({ description: 'Отзыв успешно удалён' })
+  @ApiBadRequestResponse({ description: 'Не указан Id отзыва' })
+  @ApiUnauthorizedResponse({
+    description: 'нет токена авторизации',
+  })
   @Delete('delete')
   @UseGuards(JwtAuthGuard)
   deleteFeedback(@Request() { user }, @Body() dto: DeleteFeedbackDto) {
