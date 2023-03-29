bd148f948d715879bfccdb4e7dfd5e4dec8ee01d
TMWAG <semyon.valyaev@gmail.com>
2023-03-29 23:14:17 +0300

    Базовые CRUD операции

--- src/modules/product-characteristic/product-characteristic.repository.ts
+++ src/modules/product-characteristic/product-characteristic.repository.ts
@@ -1,7 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { ProductCharacteristic } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class ProductCharacteristicRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  //create
+  async createProductCharacteristic(
+    parameterId: number,
+    productId: number,
+    value: string,
+  ): Promise<ProductCharacteristic> {
+    return await this.prisma.productCharacteristic.create({
+      data: { parameterId, productId, value },
+    });
+  }
+
+  //get
+  async getProductCharacteristicById(
+    id: number,
+  ): Promise<ProductCharacteristic> {
+    return await this.prisma.productCharacteristic.findFirst({ where: { id } });
+  }
+
+  async getAllProductCharacteristicsByProductId(
+    productId: number,
+  ): Promise<ProductCharacteristic[]> {
+    return await this.prisma.productCharacteristic.findMany({
+      where: { productId },
+    });
+  }
+
+  async getAllProductCharacteristicsByParameterId(
+    parameterId: number,
+  ): Promise<ProductCharacteristic[]> {
+    return await this.prisma.productCharacteristic.findMany({
+      where: { parameterId },
+    });
+  }
+
+  //update
+  async updateProductCharacteristicById(
+    id: number,
+    parameterId?: number,
+    productId?: number,
+    value?: string,
+  ): Promise<ProductCharacteristic> {
+    return await this.prisma.productCharacteristic.update({
+      where: { id },
+      data: { parameterId, productId, value },
+    });
+  }
+
+  //delete
+  async deleteProductCharacteristicById(
+    id: number,
+  ): Promise<ProductCharacteristic> {
+    return await this.prisma.productCharacteristic.delete({ where: { id } });
+  }
 }
