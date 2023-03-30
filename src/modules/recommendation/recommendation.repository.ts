804acf65c47935128240abfe863bc67304fc4385
TMWAG <semyon.valyaev@gmail.com>
2023-03-30 20:31:31 +0300

    Базовые CRUD операции

--- src/modules/recommendation/recommendation.repository.ts
+++ src/modules/recommendation/recommendation.repository.ts
@@ -1,7 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { Recommendation } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class RecommendationRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  //create
+  async createRecommendation(
+    product: string,
+    filename: string,
+    rating: number,
+    comment: string,
+    vendorId: number,
+  ): Promise<Recommendation> {
+    return await this.prisma.recommendation.create({
+      data: { product, filename, rating, comment, vendorId },
+    });
+  }
+
+  //get
+  async getRecommendationById(id: number): Promise<Recommendation> {
+    return await this.prisma.recommendation.findFirst({ where: { id } });
+  }
+
+  async getAllRecommendations(): Promise<Recommendation[]> {
+    return await this.prisma.recommendation.findMany();
+  }
+
+  async getAllRecommendationsByVendorId(
+    vendorId: number,
+  ): Promise<Recommendation[]> {
+    return await this.prisma.recommendation.findMany({ where: { vendorId } });
+  }
+
+  //update
+  async updateRecommendationById(
+    id: number,
+    filename?: string,
+    rating?: number,
+    comment?: string,
+    vendorId?: number,
+  ): Promise<Recommendation> {
+    return await this.prisma.recommendation.update({
+      where: { id },
+      data: { filename, rating, comment, vendorId },
+    });
+  }
+
+  //delete
+  async deleteRecommendationById(id: number): Promise<Recommendation> {
+    return this.prisma.recommendation.delete({ where: { id } });
+  }
 }
