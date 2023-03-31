0f87a8a5a0229e7560f92e2ed5c58fc200fff83a
TMWAG <semyon.valyaev@gmail.com>
2023-03-31 21:29:49 +0300

    базовые CRUD операции

--- src/modules/delivery-service/delivery-service.repository.ts
+++ src/modules/delivery-service/delivery-service.repository.ts
@@ -1,7 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { DeliveryService } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class DeliveryServiceRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  //create
+  async create(name: string): Promise<DeliveryService> {
+    return await this.prisma.deliveryService.create({ data: { name } });
+  }
+
+  //get
+  async getOneById(id: number): Promise<DeliveryService | undefined> {
+    return await this.prisma.deliveryService.findFirst({ where: { id } });
+  }
+
+  async getAll(): Promise<DeliveryService[]> {
+    return await this.prisma.deliveryService.findMany();
+  }
+
+  //update
+  async update(id: number, name: string): Promise<DeliveryService> {
+    return await this.prisma.deliveryService.update({
+      where: { id },
+      data: { name },
+    });
+  }
+
+  //delete
+  async delete(id: number): Promise<DeliveryService> {
+    return await this.prisma.deliveryService.delete({ where: { id } });
+  }
 }
