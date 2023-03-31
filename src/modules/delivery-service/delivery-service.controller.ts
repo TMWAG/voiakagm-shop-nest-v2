41beadaa410d613a58f0fe35bcc5a3fcf93aab9f
TMWAG <semyon.valyaev@gmail.com>
2023-03-31 23:02:19 +0300

    Документация swagger

--- src/modules/delivery-service/delivery-service.controller.ts
+++ src/modules/delivery-service/delivery-service.controller.ts
@@ -7,6 +7,16 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 import { Roles } from 'src/decorators/roles-auth.decorator';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -14,13 +24,24 @@ import { DeliveryServiceService } from './delivery-service.service';
 import { CreateDeliveryServiceDto } from './dto/create-delivery-service.dto';
 import { DeleteDeliveryServiceDto } from './dto/delete-delevery-service.dto';
 import { UpdateDeliveryServiceDto } from './dto/update-delivery-service.dto';
+import { DeliveryServiceEntity } from './entities/delivery-service.entity';
 
+@ApiTags('Delivery Service')
 @Controller('delivery_service')
 export class DeliveryServiceController {
   constructor(
     private readonly deliveryServiceService: DeliveryServiceService,
   ) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Добавление службы доставки' })
+  @ApiOkResponse({
+    description: 'Успешное создание службы доставки',
+    type: DeliveryServiceEntity,
+  })
+  @ApiBadRequestResponse({ description: 'Не указано название службы доставки' })
+  @ApiUnauthorizedResponse({ description: 'Нет токена авторизации' })
+  @ApiForbiddenResponse({ description: 'Роль не соответствует заданным' })
   @Post('')
   @Roles(Role.ADMIN)
   @UseGuards(RolesGuard)
@@ -28,11 +49,26 @@ export class DeliveryServiceController {
     return this.deliveryServiceService.create(dto);
   }
 
+  @ApiOperation({ summary: 'Получение всех служб доставки' })
+  @ApiOkResponse({
+    description: 'Успешное получение всех служб доставки',
+    type: [DeliveryServiceEntity],
+  })
   @Get('')
   getAll() {
     return this.deliveryServiceService.getAll();
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Изменение названия службы доставки' })
+  @ApiOkResponse({
+    description: 'Название службы доставки успешно изменено',
+    type: DeliveryServiceEntity,
+  })
+  @ApiBadRequestResponse({ description: 'Не указан Id или новое название' })
+  @ApiNotFoundResponse({ description: 'Не найдена службы доставки' })
+  @ApiUnauthorizedResponse({ description: 'Нет токена авторизации' })
+  @ApiForbiddenResponse({ description: 'Роль не соответствует заданным' })
   @Patch('')
   @Roles(Role.ADMIN)
   @UseGuards(RolesGuard)
@@ -40,6 +76,16 @@ export class DeliveryServiceController {
     return this.deliveryServiceService.update(dto);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Удаление службы доставки' })
+  @ApiOkResponse({
+    description: 'Успешное удаление службы доставки',
+    type: DeliveryServiceEntity,
+  })
+  @ApiBadRequestResponse({ description: 'Не указан Id' })
+  @ApiNotFoundResponse({ description: 'Не найдена службы доставки' })
+  @ApiUnauthorizedResponse({ description: 'Нет токена авторизации' })
+  @ApiForbiddenResponse({ description: 'Роль не соответствует заданным' })
   @Delete('')
   @Roles(Role.ADMIN)
   @UseGuards(RolesGuard)
