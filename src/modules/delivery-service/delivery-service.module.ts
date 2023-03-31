9f458a8f2cf03e9fb00f19cb5ddb3531eb9cb0bc
TMWAG <semyon.valyaev@gmail.com>
2023-03-31 22:29:24 +0300

    импорт jwt модуля

--- src/modules/delivery-service/delivery-service.module.ts
+++ src/modules/delivery-service/delivery-service.module.ts
@@ -3,10 +3,11 @@ import { DeliveryServiceService } from './delivery-service.service';
 import { DeliveryServiceController } from './delivery-service.controller';
 import { DeliveryServiceRepository } from './delivery-service.repository';
 import { PrismaModule } from 'src/database/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [DeliveryServiceService, DeliveryServiceRepository],
   controllers: [DeliveryServiceController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule],
 })
 export class DeliveryServiceModule {}
