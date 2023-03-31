c384eeeea0a0a6da9e54b30204e4ed8f006f47fa
TMWAG <semyon.valyaev@gmail.com>
2023-03-31 22:40:38 +0300

    новые эндпоинты

--- src/modules/delivery-service/delivery-service.controller.ts
+++ src/modules/delivery-service/delivery-service.controller.ts
@@ -1,9 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/decorators/roles-auth.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { DeliveryServiceService } from './delivery-service.service';
+import { CreateDeliveryServiceDto } from './dto/create-delivery-service.dto';
+import { DeleteDeliveryServiceDto } from './dto/delete-delevery-service.dto';
+import { UpdateDeliveryServiceDto } from './dto/update-delivery-service.dto';
 
-@Controller('delivery-service')
+@Controller('delivery_service')
 export class DeliveryServiceController {
   constructor(
     private readonly deliveryServiceService: DeliveryServiceService,
   ) {}
+
+  @Post('')
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  create(@Body() dto: CreateDeliveryServiceDto) {
+    return this.deliveryServiceService.create(dto);
+  }
+
+  @Get('')
+  getAll() {
+    return this.deliveryServiceService.getAll();
+  }
+
+  @Patch('')
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  update(@Body() dto: UpdateDeliveryServiceDto) {
+    return this.deliveryServiceService.update(dto);
+  }
+
+  @Delete('')
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  delete(@Body() dto: DeleteDeliveryServiceDto) {
+    return this.deliveryServiceService.delete(dto);
+  }
 }
