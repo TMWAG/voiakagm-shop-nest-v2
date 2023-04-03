042d603cb8fd8738c4f3579e1981191fafb764f6
TMWAG <semyon.valyaev@gmail.com>
2023-04-03 18:38:52 +0300

    методы для создания и получения, утилитарный метод для создания обекта параметров

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,7 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { GetAllOrdersDto } from './dto/get-all-orders.dto';
 import { OrderRepository } from './order.repository';
 
 @Injectable()
 export class OrderService {
   constructor(private readonly repository: OrderRepository) {}
+
+  //create
+  async createOrGetCurrent(userId: number) {
+    const order = await this.repository.getCurrentByUserId(userId);
+    if (!order) {
+      return await this.repository.create(userId);
+    }
+    return order;
+  }
+
+  //get
+  async getAll(dto: GetAllOrdersDto) {
+    return await this.repository.getAll(
+      this.constructSearchOptionsFromDto(dto),
+    );
+  }
+
+  async getAllUser(userId: number) {
+    return await this.repository.getAllByUserId(userId);
+  }
+
+  //update
+
+  //utils
+  private constructSearchOptionsFromDto(
+    dto: GetAllOrdersDto,
+  ): Prisma.OrderFindManyArgs {
+    const searchOptions: Prisma.OrderFindManyArgs = {};
+    const take = dto.limit || 12;
+    searchOptions.take = take;
+    const page = dto.page || 1;
+    searchOptions.skip = page * take - take;
+    searchOptions.orderBy = { createdAt: 'asc' };
+    searchOptions.where = {
+      userId: dto.userId || undefined,
+      deliveryServiceId: dto.deliveryServiceId || undefined,
+      status: dto.status || undefined,
+    };
+    searchOptions.include = {
+      deliveryService: true,
+      userAddress: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          surname: true,
+          phone: true,
+          email: true,
+          vkLink: true,
+          tgLink: true,
+        },
+      },
+    };
+    return searchOptions;
+  }
 }
