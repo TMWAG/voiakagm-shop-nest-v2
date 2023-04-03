c2047491093f7486e23741445773499129404435
TMWAG <semyon.valyaev@gmail.com>
2023-04-03 21:17:43 +0300

    импорт модуля заказов

--- src/modules/ordered-products/ordered-products.module.ts
+++ src/modules/ordered-products/ordered-products.module.ts
@@ -3,10 +3,11 @@ import { OrderedProductsService } from './ordered-products.service';
 import { OrderedProductsController } from './ordered-products.controller';
 import { OrderedProductsRepository } from './ordered-products.repository';
 import { PrismaModule } from 'src/database/prisma.module';
+import { OrderModule } from '../order/order.module';
 
 @Module({
   providers: [OrderedProductsService, OrderedProductsRepository],
   controllers: [OrderedProductsController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, OrderModule],
 })
 export class OrderedProductsModule {}
