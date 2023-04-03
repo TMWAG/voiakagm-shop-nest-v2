56733d9ae16c7d50d793bcbd3ee471130bddc403
TMWAG <semyon.valyaev@gmail.com>
2023-04-03 19:40:45 +0300

    CRUD операции

--- src/modules/ordered-products/ordered-products.repository.ts
+++ src/modules/ordered-products/ordered-products.repository.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { OrderedProduct } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class OrderedProductsRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  //create
+  async create(
+    orderId: number,
+    productId: number,
+    amount: number,
+  ): Promise<OrderedProduct> {
+    return await this.prisma.orderedProduct.create({
+      data: { orderId, productId, amount },
+    });
+  }
+
+  //get
+  async getOne(id: number): Promise<OrderedProduct> {
+    return await this.prisma.orderedProduct.findFirst({ where: { id } });
+  }
+
+  //update
+  async updateAmount(id: number, amount: number): Promise<OrderedProduct> {
+    return await this.prisma.orderedProduct.update({
+      where: { id },
+      data: { amount },
+    });
+  }
+
+  //delete
+  async delete(id: number): Promise<OrderedProduct> {
+    return await this.prisma.orderedProduct.delete({ where: { id } });
+  }
 }
