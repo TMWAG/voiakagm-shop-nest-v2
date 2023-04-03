95be36817e96f7f81b271ddbdf23742b1e71303d
TMWAG <semyon.valyaev@gmail.com>
2023-04-03 21:20:28 +0300

    методы добавления, изменения и удаления товаров корзины

--- src/modules/ordered-products/ordered-products.service.ts
+++ src/modules/ordered-products/ordered-products.service.ts
@@ -1,7 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { OrderService } from '../order/order.service';
+import { CreateOrderedProductDto } from './dto/create-ordered-product.dto';
+import { DeleteOrderedProductDto } from './dto/delete-ordered-product.dto';
+import { UpdateOrderedProductAmountDto } from './dto/update-ordered-product-amount.dto';
 import { OrderedProductsRepository } from './ordered-products.repository';
 
 @Injectable()
 export class OrderedProductsService {
-  constructor(private readonly repository: OrderedProductsRepository) {}
+  constructor(
+    private readonly repository: OrderedProductsRepository,
+    private readonly orderService: OrderService,
+  ) {}
+
+  //create
+  //add product to cart
+  async create(userId: number, dto: CreateOrderedProductDto) {
+    const order = await this.orderService.createOrGetCurrent(userId);
+    return await this.repository.create(order.id, dto.productId, dto.amount);
+  }
+
+  //get
+  async getOneOrThrowError(id: number) {
+    const orderedProduct = await this.repository.getOne(id);
+    if (!orderedProduct)
+      throw new NotFoundException(`Товар корзины с id ${id} не найден`);
+    return orderedProduct;
+  }
+
+  //update
+  async updateAmount(dto: UpdateOrderedProductAmountDto) {
+    await this.getOneOrThrowError(dto.id);
+    return await this.repository.updateAmount(dto.id, dto.amount);
+  }
+
+  //delete
+  async delete(dto: DeleteOrderedProductDto) {
+    await this.getOneOrThrowError(dto.id);
+    return await this.repository.delete(dto.id);
+  }
 }
