35d689f09046eb29e52d95f01ba4847ce39d0744
TMWAG <semyon.valyaev@gmail.com>
2023-04-04 16:54:25 +0300

    эндпоинт для получения информации о заказе по id

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { Role } from '@prisma/client';
 import { Roles } from 'src/decorators/roles-auth.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { GetAllOrdersDto } from './dto/get-all-orders.dto';
+import { GetOneOrderDto } from './dto/get-one-order.dto';
 import { OrderService } from './order.service';
 
 @Controller('order')
@@ -15,6 +23,13 @@ export class OrderController {
   @UseGuards(RolesGuard)
   getAll(@Query() dto: GetAllOrdersDto) {
     return this.orderService.getAll(dto);
+  }
+
+  @Get(':id')
+  @Roles(Role.ADMIN, Role.SUPERVISOR)
+  @UseGuards(RolesGuard)
+  getOne(@Param() dto: GetOneOrderDto) {
+    return this.orderService.getOne(dto);
   }
 
   @Get('user')
