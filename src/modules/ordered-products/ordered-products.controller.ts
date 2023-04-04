e56f0bceb86245dbc44f8f5ee13bf1c4c006aa67
TMWAG <semyon.valyaev@gmail.com>
2023-04-04 17:21:39 +0300

    эндпоинты для добавления, изменения кол-ва и удаления товаров в корзину

--- src/modules/ordered-products/ordered-products.controller.ts
+++ src/modules/ordered-products/ordered-products.controller.ts
@@ -1,9 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Patch,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreateOrderedProductDto } from './dto/create-ordered-product.dto';
+import { DeleteOrderedProductDto } from './dto/delete-ordered-product.dto';
+import { UpdateOrderedProductAmountDto } from './dto/update-ordered-product-amount.dto';
 import { OrderedProductsService } from './ordered-products.service';
 
-@Controller('ordered-products')
+@Controller('cart')
 export class OrderedProductsController {
   constructor(
     private readonly orderedProductsService: OrderedProductsService,
   ) {}
+
+  @Post('')
+  @UseGuards(JwtAuthGuard)
+  add(@Request() { user }, @Body() dto: CreateOrderedProductDto) {
+    return this.orderedProductsService.create(user.id, dto);
+  }
+
+  @Patch('')
+  @UseGuards(JwtAuthGuard)
+  changeAmount(@Body() dto: UpdateOrderedProductAmountDto) {
+    return this.orderedProductsService.updateAmount(dto);
+  }
+
+  @Delete('')
+  @UseGuards(JwtAuthGuard)
+  delete(@Body() dto: DeleteOrderedProductDto) {
+    return this.orderedProductsService.delete(dto);
+  }
 }
