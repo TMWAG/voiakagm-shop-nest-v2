d86575ff93e6f652be8c38444e909cd497b1b73b
TMWAG <semyon.valyaev@gmail.com>
2023-04-05 15:02:20 +0300

    импорт модуля пользовательских адресов

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -4,11 +4,12 @@ import { OrderController } from './order.controller';
 import { OrderRepository } from './order.repository';
 import { PrismaModule } from 'src/database/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersAddressesModule } from '../users-addresses/users-addresses.module';
 
 @Module({
   providers: [OrderService, OrderRepository],
   controllers: [OrderController],
-  imports: [PrismaModule, JwtModule],
+  imports: [PrismaModule, JwtModule, UsersAddressesModule],
   exports: [OrderService],
 })
 export class OrderModule {}
