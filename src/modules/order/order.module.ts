7ed72871e922c2a1bbab9038cc197daf2dca6a0c
TMWAG <semyon.valyaev@gmail.com>
2023-04-11 23:31:32 +0300

    импорт модуля эквайринга

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -5,11 +5,12 @@ import { OrderRepository } from './order.repository';
 import { PrismaModule } from 'src/database/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersAddressesModule } from '../users-addresses/users-addresses.module';
+import { TinkoffAcqModule } from '../tinkoff-acq/tinkoff-acq.module';
 
 @Module({
   providers: [OrderService, OrderRepository],
   controllers: [OrderController],
-  imports: [PrismaModule, JwtModule, UsersAddressesModule],
+  imports: [PrismaModule, JwtModule, UsersAddressesModule, TinkoffAcqModule],
   exports: [OrderService],
 })
 export class OrderModule {}
