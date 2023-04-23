b491f3df4a813c7a629fcb45f360f9c0a9bedba2
TMWAG <semyon.valyaev@gmail.com>
2023-04-23 20:19:56 +0300

    импорт модуля приобретённых товаров

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -6,11 +6,18 @@ import { PrismaModule } from 'src/database/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersAddressesModule } from '../users-addresses/users-addresses.module';
 import { TinkoffAcqModule } from '../tinkoff-acq/tinkoff-acq.module';
+import { PurchasedProductsModule } from '../purchased-products/purchased-products.module';
 
 @Module({
   providers: [OrderService, OrderRepository],
   controllers: [OrderController],
-  imports: [PrismaModule, JwtModule, UsersAddressesModule, TinkoffAcqModule],
+  imports: [
+    PrismaModule,
+    JwtModule,
+    UsersAddressesModule,
+    TinkoffAcqModule,
+    PurchasedProductsModule,
+  ],
   exports: [OrderService],
 })
 export class OrderModule {}
