4f5e838052f852e4c568694ce2a6477f260dfae0
TMWAG <semyon.valyaev@gmail.com>
2023-04-26 20:27:17 +0300

    новые сущности для документации

--- src/modules/ordered-products/entities/ordered-product.entity.ts
+++ src/modules/ordered-products/entities/ordered-product.entity.ts
@@ -1,34 +1,48 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-type VendorType = {
+class VendorType {
+  @ApiProperty({ example: 1 })
   id: number;
+  @ApiProperty({ example: 'MSI' })
   name: string;
-};
-type CategoryType = {
+}
+
+class CategoryType {
+  @ApiProperty({ example: 2 })
   id: number;
+  @ApiProperty({ example: 'Видеокарты' })
   name: string;
-};
-type PictureType = {
-  id: number;
-  productId: number;
+}
+
+class PictureType {
+  @ApiProperty({ example: 'e45208dd-5d43-4b89-b6fe-374acb643089.jpg' })
   filename: string;
-};
-type ProductType = {
+}
+
+class ProductType {
+  @ApiProperty({ example: 4 })
   id: number;
+  @ApiProperty({ example: 'RTX 3070' })
   name: string;
+  @ApiProperty({ example: 12400000 })
   price: number;
+  @ApiProperty({ example: 12 })
+  discount: number;
+  @ApiProperty({ type: VendorType })
   vendor: VendorType;
+  @ApiProperty({ type: CategoryType })
   category: CategoryType;
+  @ApiProperty({ type: PictureType })
   picture: PictureType[];
-};
+}
 
 export class OrderedProductEntity {
-  @ApiProperty()
+  @ApiProperty({ example: 5 })
   id: number;
-  @ApiProperty()
+  @ApiProperty({ example: 9 })
   orderId: number;
-  @ApiProperty()
+  @ApiProperty({ example: 6 })
   amount: number;
-  @ApiProperty()
+  @ApiProperty({ type: ProductType })
   product: ProductType;
 }
---
+++ src/modules/ordered-products/entities/update-ordered-product.entity.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateOrderedProductEntity {
+  @ApiProperty({ example: 1 })
+  id: number;
+  @ApiProperty({ example: 213 })
+  orderId: number;
+  @ApiProperty({ example: 43 })
+  productId: number;
+  @ApiProperty({ example: 2 })
+  amount: number;
+}
