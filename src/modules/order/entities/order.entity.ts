8635afa25364537b581d51f1fd1a53d4072e1b73
TMWAG <semyon.valyaev@gmail.com>
2023-04-26 17:22:43 +0300

    пеработана сущность для документации

--- src/modules/order/entities/order.entity.ts
+++ src/modules/order/entities/order.entity.ts
@@ -3,69 +3,91 @@ import { OrderStatus } from '@prisma/client';
 import { CategoryEntity } from 'src/modules/category/entities/category.entity';
 import { VendorEntity } from 'src/modules/vendor/entities/vendor.entity';
 
-type UserType = {
+class UserType {
+  @ApiProperty({ example: 'Сергей' })
   name: string;
+  @ApiProperty({ example: 'Колесников' })
   surname: string;
+  @ApiProperty({ example: 'example@mail.me' })
   email: string;
+  @ApiProperty({ example: '+79991112233' })
   phone: string;
+  @ApiProperty({ example: 'https://vk.com/sergeyCol' })
   vkLink: string;
+  @ApiProperty({ example: '@serCol' })
   tgLink: string;
-};
+}
 
-type UserAddressType = {
-  address: string;
-};
+class UserAddressType {
+  @ApiProperty({
+    example: 'Россия, Московская Область, Королёв, Трудовая, 12, 104',
+  })
+  address: string | null;
+}
 
-type DeliveryServiceType = {
-  address: string;
-};
-
-type PictureType = {
+class DeliveryServiceType {
+  @ApiProperty({ example: 1 })
   id: number;
-  productId: number;
+  @ApiProperty({ example: 'Почта России' })
+  name: string;
+}
+
+class PictureType {
+  @ApiProperty({
+    example: 'e45208dd-5d43-4b89-b6fe-374acb643089.jpg',
+  })
   filename: string;
-};
+}
 
-type Product = {
+class Product {
+  @ApiProperty({ example: 1 })
   id: number;
+  @ApiProperty({ example: 'RTX 3070' })
   name: number;
+  @ApiProperty()
   vendor: VendorEntity;
+  @ApiProperty()
   category: CategoryEntity;
+  @ApiProperty({ example: 5000000 })
   price: number;
+  @ApiProperty({ example: 12 })
   discount: number | null;
+  @ApiProperty({ example: false })
   used: boolean;
-  amount: number;
+  @ApiProperty({ type: PictureType })
   pictures: PictureType[];
-};
+}
 
-type OrderedProduct = {
+class OrderedProduct {
+  @ApiProperty()
   product: Product;
+  @ApiProperty({ example: 1 })
   amount: number;
-};
+}
 
 export class OrderEntity {
-  @ApiProperty()
+  @ApiProperty({ example: 1 })
   id: number;
 
-  @ApiProperty()
+  @ApiProperty({ example: 2 })
   userId: number;
 
-  @ApiProperty()
+  @ApiProperty({ example: 3 })
   userAddressId: number | null;
 
-  @ApiProperty()
+  @ApiProperty({ example: '2023-04-04T12:07:04.006Z' })
   createdAt: string;
 
-  @ApiProperty()
+  @ApiProperty({ enum: OrderStatus })
   status: OrderStatus;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'RW01230934434EX' })
   trackNo: string | null;
 
-  @ApiProperty()
+  @ApiProperty({ example: 1 })
   deliveryServiceId: number | null;
 
-  @ApiProperty()
+  @ApiProperty({ example: true })
   crate: boolean;
 
   @ApiProperty()
@@ -77,6 +99,6 @@ export class OrderEntity {
   @ApiProperty()
   deliveryService: DeliveryServiceType | null;
 
-  @ApiProperty()
+  @ApiProperty({ type: [OrderedProduct] })
   orderedProducts: OrderedProduct[];
 }
