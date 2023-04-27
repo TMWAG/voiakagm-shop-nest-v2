5804260cd4e80ff24c2d1031d8d1fe6f41a98334
TMWAG <semyon.valyaev@gmail.com>
2023-04-27 20:53:21 +0300

    импорт модуля категорий

--- src/modules/parameter/parameter.module.ts
+++ src/modules/parameter/parameter.module.ts
@@ -4,12 +4,14 @@ import { ParameterController } from './parameter.controller';
 import { ParameterRepository } from './parameter.repository';
 import { PrismaModule } from 'src/database/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
   providers: [ParameterService, ParameterRepository],
   controllers: [ParameterController],
   imports: [
     PrismaModule,
+    CategoryModule,
     JwtModule.register({
       secret: process.env.PRIVATE_KEY,
       signOptions: { expiresIn: '24h' },
