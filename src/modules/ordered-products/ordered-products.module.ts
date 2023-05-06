382c1124ebcef2452b4257589912b14e3a042555
TMWAG <semyon.valyaev@gmail.com>
2023-05-06 19:41:35 +0300

    добавлен импорт модуля товаров

--- src/modules/ordered-products/ordered-products.module.ts
+++ src/modules/ordered-products/ordered-products.module.ts
@@ -4,10 +4,11 @@ import { OrderedProductsController } from './ordered-products.controller';
 import { OrderedProductsRepository } from './ordered-products.repository';
 import { PrismaModule } from 'src/database/prisma.module';
 import { OrderModule } from '../order/order.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
   providers: [OrderedProductsService, OrderedProductsRepository],
   controllers: [OrderedProductsController],
-  imports: [PrismaModule, OrderModule],
+  imports: [PrismaModule, OrderModule, ProductModule],
 })
 export class OrderedProductsModule {}
